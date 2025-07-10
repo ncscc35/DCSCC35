0f04e6f7e3d33947ef5c48b921d0ca89bc300af2
ncscc35 <luckyzhari1@gmail.com>
2025-07-10 14:32:42 +0530

    commit

---
+++ hari.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
